2e0d1b3bf219935aa3707665f6a63bb4f8c33b78
tvthanh <truongvanthanhagu@gmail.com>
2015-04-12 21:18:14 +0700

    Commit second time

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,8 +1,48 @@
 function AppCtrl($scope, $http) {
 	console.log("Hello world from controller");
 
-	$http.get('/contactlist').success(function(response) {
-		console.log("I got the data I requested");
-		$scope.contactlist = response;
-	});
+	var refresh = function() {
+		$http.get('/contactlist').success(function(response) {
+			console.log("I got the data I requested");
+			$scope.contactlist = response;
+			$scope.contact = "";
+		});
+	}
+
+	refresh();
+
+	$scope.addContact = function() {
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response) {
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.remove = function(id) {
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.edit = function(id) {
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function(response) {
+			$scope.contact = response;
+			// refresh();
+		});
+	};
+
+	$scope.update = function() {
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.deselect = function() {
+		$scope.contact = "";
+	}
+
 }
--- server.js
+++ server.js
@@ -1,34 +1,76 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
 
+var db = mongojs('contactlist', ['contactlist']);
+
+var bodyParser = require('body-parser');
 // app.get('/', function(req, res) {
 // 	res.send("Hello world from server.js");
 // });
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res) {
 	console.log("I received a GET request");
-	person1 = 
-		{
-			name: 'Tim',
-			email: 'tim@email.com',
-			number: '(111)-111-1111'
-		};
-	person2 = 
-		{
-			name: 'Emily',
-			email: 'emily@email.com',
-			number: '(222)-222-2222'
-		};
-	person3 = 
-		{
-			name: 'John',
-			email: 'john@email.com',
-			number: '(333)-333-3333'
-		};
-	var contactlist = [person1, person2, person3];
-	res.json(contactlist);
+	// person1 = 
+	// 	{
+	// 		name: 'Tim',
+	// 		email: 'tim@email.com',
+	// 		number: '(111)-111-1111'
+	// 	};
+	// person2 = 
+	// 	{
+	// 		name: 'Emily',
+	// 		email: 'emily@email.com',
+	// 		number: '(222)-222-2222'
+	// 	};
+	// person3 = 
+	// 	{
+	// 		name: 'John',
+	// 		email: 'john@email.com',
+	// 		number: '(333)-333-3333'
+	// 	};
+	// var contactlist = [person1, person2, person3];
+	// res.json(contactlist);
+	db.contactlist.find(function(err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/contactlist', function(req, res) {
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.get('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+		new: true }, function(err, doc) {
+			res.json(doc);
+	});
 });
 
 app.listen(3000);
